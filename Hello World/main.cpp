4b5d024f3f49ef74e1f06861ed4323af174dfa49
Aneta92 <45732533+Aneta92@users.noreply.github.com>
2018-12-09 13:27:40 +0100

    Vol. 3
    Zmiana dotycząca zamieszkania i pytanie o imie

--- Hello World/main.cpp
+++ Hello World/main.cpp
@@ -5,8 +5,13 @@ using namespace std;
 int main()
 {
     string country;
+    string imie;
+
     cout << "Hello world!" << endl;
-    cout <<"Where are you from?"<<endl;
+    cout <<"Podaj w jakim kraju mieszkasz: "<<endl;
     cin >> country;
+    cout << "Podaj swoje imie: "<<endl;
+    cin >> imie;
+
     return 0;
 }
