e25cb6be11528dc7075b910bed04ff0728a9b782
Aneta92 <45732533+Aneta92@users.noreply.github.com>
2018-12-09 13:38:02 +0100

    Vol. 5
    Rozpoczecie kalkulatora

--- Hello World/main.cpp
+++ Hello World/main.cpp
@@ -6,6 +6,8 @@ int main()
 {
     string country;
     string imie;
+    int a;
+    int b;
 
     cout << "Hello world!" << endl;
     cout <<"Podaj w jakim kraju mieszkasz: "<<endl;
@@ -13,6 +15,13 @@ int main()
     cout << "Podaj swoje imie: "<<endl;
     cin >> imie;
     cout << "Czesc, "<< imie <<" z kraju "<< country <<"!";
+    cout << "Witamy w kalkulatorze! Podaj dwie liczby "<<endl;
+    cout << "Liczba a: " << endl;
+    cin >> a;
+    cout << "Liczba b: " << endl;
+    cin >> a;
+
+    //Podaj numer dzialania ktore chcesz wykonac:
 
     return 0;
 }
